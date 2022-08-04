8028abf9ba5925762b6da77347dc4c5b93551d80
todelusion <todelusion.gmail.com>
2022-08-04 22:43:45 +0800

    f/prettier

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import Todolist from "./views/Todolist";
 import "./tailwind.css";
 
-
-
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Todolist />
--- src/views/Todolist.jsx
+++ src/views/Todolist.jsx
@@ -3,22 +3,24 @@ import axios from "axios";
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
 
-function Todolist(){
-  
-  return(
+function Todolist() {
+  return (
     <div>
-      <h1 className="hidden -z-10 text-6xl font-dela whitespace-nowrap md:flex">
-        A React Based To 
-        <span className="OvalContainer OvalSelf"></span>
-        Do
-      </h1>
-      <h1 className="-z-10 w-max text-5xl font-dela md:hidden justify-between">
-        A React
-      <span className="m-OvalContainer m-OvalSelf">Based</span>
-        To Do
-      </h1>
+      <nav>
+        <h1 className="-z-10 mb-14 hidden whitespace-nowrap font-dela text-6xl md:flex">
+          A React Based To
+          <span className="OvalContainer OvalSelf"></span>
+          Do
+        </h1>
+        <h1 className="-z-10 w-max justify-between font-dela text-5xl md:hidden">
+          A React
+          <span className="m-OvalContainer m-OvalSelf">Based</span>
+          To Do
+        </h1>
+      </nav>
+      <div className="relative mx-auto h-[800px] w-full max-w-3xl border-2 border-black"></div>
     </div>
-  )
+  );
 }
 
-export default Todolist
+export default Todolist;
