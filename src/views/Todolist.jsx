faf1b516329131e55396d5488b30c33dd26fb370
todelusion <todelusion@gmail.com>
2022-08-05 13:38:59 +0800

    f/add UI_todos

--- src/views/Todolist.jsx
+++ src/views/Todolist.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import axios from "axios";
+import React from 'react';
+import axios from 'axios';
+import './Todolist.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
 
@@ -18,8 +23,21 @@ function Todolist() {
           To Do
         </h1>
       </nav>
-      <div className="flex w-full justify-center">
-        <div className="RectangleContainer hover-RectangleContainer RectangleSelf hover-RectangleSelf"></div>
+      <div className="flex flex-col items-center w-full">
+        <div className="mb-16 px-16 py-10  RectangleContainer hover-RectangleContainer RectangleSelf hover-RectangleSelf">
+          <input type="text"  className="py-1 px-1 border-2 border-black rounded-md w-full outline-none"/>
+        </div>
+        <div className="px-16 py-7 RectangleContainer hover-RectangleContainer RectangleSelf hover-RectangleSelf">
+          <ul>
+            <li className="flex items-center justify-between">
+              <div className="flex items-center">
+                <FontAwesomeIcon icon={faCheck} />
+                <p className="ml-7">提醒我買USB線</p>
+              </div>
+              <FontAwesomeIcon icon={faX} className="i-faX" />
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
