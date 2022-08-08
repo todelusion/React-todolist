90ce031a70440d7a15328d590a87eb38c0648765
todelusion <todelusion.gmail.com>
2022-08-09 00:30:00 +0800

    fixed/modalUI

--- src/components/LoadingModal.jsx
+++ src/components/LoadingModal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoadingModal() {
   return (
     <div className="fixed top-0 z-10 flex h-screen w-full items-center justify-center">
-      <div className="h-20 w-32 animate-spin rounded-[100%] border-2 border-primary"></div>
+      <div className="h-14 w-20 md:h-20 md:w-32 animate-spin rounded-[100%] border-2 border-primary"></div>
     </div>
   );
 }
